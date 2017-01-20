3ebb252a3cfa780858d20afd7b4b918fb03a3688
KuroiRoy <roy.zwart@gmail.com>
2017-01-20 21:34:27 +0100

    Testing gravity line with colored object

---
+++ Assets/Scripts/ColorTester.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace GlobalGameJam2017 {
+
+	[RequireComponent(typeof(SpriteRenderer), typeof(Rigidbody2D))]
+	public class ColorTester : MonoBehaviour {
+
+		private new Rigidbody2D rigidbody;
+		private SpriteRenderer spriteRenderer;
+
+		private void Awake () {
+			rigidbody = GetComponent<Rigidbody2D>();
+			spriteRenderer = GetComponent<SpriteRenderer>();
+		}
+
+		private void Start () {
+			
+		}
+
+		private void Update () {
+//			transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * 4, 0);
+
+//			transform.position -= new Vector3(0, GravityLine.Instance.GetValue(transform.position) * Time.deltaTime * 8);
+
+			var gravity = GravityLine.Instance.GetValue(transform.position);
+
+			spriteRenderer.color = Color.Lerp(Color.green, Color.red, (gravity + 1f) / 2);
+
+			rigidbody.gravityScale = (gravity);
+			rigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * 50, Input.GetButtonUp("Jump") ? 50 : 0));
+		}
+
+		private void OnCollision2D () {
+			
+		}
+
+	}
+
+}
---
+++ Assets/Scripts/GravityLine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GlobalGameJam2017 {
+
+	public class GravityLine : MonoBehaviour {
+
+		public static GravityLine Instance { get; private set; }
+
+		void Awake () {
+			if (Instance != null) {
+				Debug.LogError("GravityLine instance already set");
+			}
+
+			Instance = this;
+		}
+
+		// Use this for initialization
+		void Start () {}
+
+		// Update is called once per frame
+		void Update () {}
+
+		public float GetValue (Vector2 pos) {
+			return Mathf.Sign(pos.y / 10);
+		}
+
+	}
+
+}
