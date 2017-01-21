7c8a53dbe2a8525e9786929ed92af75cd153c1eb
KuroiRoy <roy.zwart@gmail.com>
2017-01-21 21:32:47 +0100

    Portal

--- Assets/Scripts/PortalParticleSystem.cs
+++ Assets/Scripts/PortalParticleSystem.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(ParticleSystem))]
 public class PortalParticleSystem : MonoBehaviour {
 
-	private ParticleSystem particleSystem;
+	public float rotationSpeed = 16;
+	public float speedToCenter = 1;
+
+	private new ParticleSystem particleSystem;
 
 	void Awake () {
 		particleSystem = GetComponent<ParticleSystem>();
@@ -18,6 +21,20 @@ public class PortalParticleSystem : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//var particles = particleSystem.GetParticles();
+		ParticleSystem.Particle[] particles = new ParticleSystem.Particle[particleSystem.main.maxParticles];
+		var particleCount = particleSystem.GetParticles(particles);
+
+		for (int i = 0; i < particleCount; i++) {
+			var life = particles[i].remainingLifetime / particles[i].startLifetime;
+			var distance = Vector3.Distance(particles[i].position, transform.position);
+			var normalToCenter = -particles[i].position.normalized * speedToCenter;
+
+			var p1 = new Vector3(Mathf.Cos(Time.time + i), Mathf.Sin(Time.time + i)) * distance * life;
+			var p2 = new Vector3(Mathf.Cos(Time.time + i + 0.01f), Mathf.Sin(Time.time + i + 0.01f)) * distance * life + normalToCenter;
+
+			particles[i].velocity += (p2 - p1).normalized * Time.deltaTime * rotationSpeed;
+		}
+
+		particleSystem.SetParticles(particles, particleCount);
 	}
 }
