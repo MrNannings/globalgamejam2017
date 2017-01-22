eabb0d8020cb65d891969b114399f2e8fd0dc8e2
DESKTOP-2AMNTSS\lokaal1 <mrnannings@gmail.com>
2017-01-22 01:53:47 +0100

    Level Select screen - scene with the LevelSelect Controller script made;

---
+++ Assets/LevelSelectController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClicked(Button button)
+    {
+        //Debug.Log(button.GetComponentInChildren<Text>().text);
+        int levelNumber = int.Parse(button.GetComponentInChildren<Text>().text);
+        if (levelNumber < 10)
+        {
+            Debug.Log("level00" + levelNumber);
+            SceneManager.LoadScene("level00" + levelNumber);
+        }
+        else
+        {
+            Debug.Log("level0" + levelNumber);
+            SceneManager.LoadScene("level0" + levelNumber);
+        }
+        
+    }
+}
