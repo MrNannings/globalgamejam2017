177af4d02a5222a2625d5aadb00c4464cf00b8d4
KuroiRoy <roy.zwart@gmail.com>
2017-01-22 13:44:05 +0100

    Automatically generate enemies

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public GameObject EnemyPrefab;
+
+	// Use this for initialization
+	void Start () {
+		var horizontalObstacles = GameObject.FindGameObjectsWithTag("horizontalobstacle");
+		var verticalObstacles = GameObject.FindGameObjectsWithTag("verticalobstacle");
+
+		foreach (var obstacle in horizontalObstacles) {
+			Create(obstacle, "right");
+		}
+		foreach (var obstacle in verticalObstacles) {
+			Create(obstacle, "up");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Create (GameObject obstacle, string direction) {
+		var boxCollider = obstacle.GetComponent<BoxCollider2D>();
+
+		var enemy = Instantiate(EnemyPrefab).transform;
+		enemy.position = boxCollider.bounds.center;
+
+		var controller = enemy.GetComponent<EnemyController>();
+		controller.direction = direction;
+		controller.boxColliderPath = boxCollider;
+	}
+}
